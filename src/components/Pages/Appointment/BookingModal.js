d96d26ae8474294717795f9e3e7689d2a757da98
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-09-08 10:30:55 +0600

    Query initialization

--- src/components/Pages/Appointment/BookingModal.js
+++ src/components/Pages/Appointment/BookingModal.js
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading';
 
@@ -13,17 +14,43 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
     }
 
     const { _id, name, slots } = treatment;
+    const formattedDate = format(date, 'PP');
 
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
+        const phone = event.target.phone.value;
 
-        console.log(_id, name, slot);
-        
-        // to close the modal
-        setTreatment(null);
+        const booking = {
+            treatmentId: _id,
+            treatment: name,
+            date: formattedDate,
+            patient: user.email,
+            slot,
+            patientName: user.displayName,
+            phone
+        }
+        console.log(booking);
+
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    toast.success(`Appointment is set, ${formattedDate} at ${slot}`)
+                }
+                else {
+                    toast.error(`Already have and appointment on ${data.booking?.date} at ${data.booking?.slot}`)
+                }
+                setTreatment(null);
+            });
     };
-    
+
     return (
         <div >
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -40,7 +67,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                         </select>
                         <input type="text" name="name" value={user?.displayName || ''} disabled className="input input-bordered w-full max-w-xs" />
                         <input type="email" name="email" value={user?.email || ''} disabled className="input input-bordered w-full max-w-xs" />
-                        <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="phone" placeholder="Phone Number" required className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
                 </div>
--- src/components/Pages/Home/Info.js
+++ src/components/Pages/Home/Info.js
@@ -7,9 +7,9 @@ import phone from '../../../assets/icons/phone.svg';
 const Info = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-12 font-bold my-10'>
-            <InfoCart cartTitle='Opening Hours' bgclassName='bg-gradient-to-r from-secondary to-primary' img={clock} />
-            <InfoCart cartTitle='Our Location' bgclassName='bg-accent' img={marker} />
-            <InfoCart cartTitle='Contuct Us' bgclassName='bg-gradient-to-r from-secondary to-primary' img={phone} />
+            <InfoCart cartTitle='Opening Hours' bgClass='bg-gradient-to-r from-secondary to-primary' img={clock} />
+            <InfoCart cartTitle='Our Location' bgClass='bg-accent' img={marker} />
+            <InfoCart cartTitle='Contuct Us' bgClass='bg-gradient-to-r from-secondary to-primary' img={phone} />
         </div>
     );
 };
